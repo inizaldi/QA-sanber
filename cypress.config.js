554c22e0440fa9884cc180eb64cd99dafd669341
inizaldi <rizaldiakbar57@gmail.com>
2023-11-30 15:08:48 +0700

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://magento.softwaretestingboard.com/",
+    env: {
+      Valid_email: "buattest@gmail.com",
+      Valid_password: "test123*",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
